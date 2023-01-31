c3d9862bb7f8bfa4a583940097a5bd481c524c95
Albert G <516972+alber70g@users.noreply.github.com>
2023-01-31 11:23:31 +0100

    chore(graph): clean comments

--- packages/apps/graph/src/graph/Query/lastBlockHeight.ts
+++ packages/apps/graph/src/graph/Query/lastBlockHeight.ts
@@ -1,45 +1,5 @@
-// schema: createSchema({
-//   typeDefs: [
-//     BigIntTypeDefinition,
-//     DateTypeDefinition,
-//     PositiveFloatTypeDefinition,
-//     loadFileAsString('./schema.graphql'),
-//   ],
-
 import { prismaClient } from '../../db/prismaClient';
 import { builder } from '../builder';
-
-//   resolvers: {
-//     Query: {
-//       hello: (_, args) => {
-//         return {
-//           id: '1',
-//           name: 'hello',
-//         };
-//       },
-//       lastBlockHeight: async (parent, args, context) => {
-//         const lastBlock = await context.prisma.blocks.findFirst({
-//           orderBy: {
-//             height: 'desc',
-//           },
-//         });
-//         return lastBlock?.height;
-//       },
-//     },
-
-//     Block: {
-//       chainid: (parent) => BigInt(parent.chainid),
-//       height: (parent) => BigInt(parent.height),
-//     },
-
-//     Subscription: {
-//       newBlocks: {
-//         subscribe: () => pubsub.subscribe('NEW_BLOCKS'),
-//         resolve: (payload) => payload,
-//       },
-//     },
-//   },
-// }),
 
 builder.queryField('lastBlockHeight', (t) => {
   return t.field({
